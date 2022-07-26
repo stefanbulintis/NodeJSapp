46c489bd7fc81cc6cc54989e8b81f13a4d5e99be
Bulintis Stefan Claudiu <stefan.bulintis@student.upt.ro>
2022-07-26 18:02:26 +0300

    FIRST COMMIT

---
+++ server/model/model.js
@@ -0,0 +1,20 @@
+const { MongoGridFSChunkError } = require('mongodb');
+const mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender: String,
+    status: String
+})
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
